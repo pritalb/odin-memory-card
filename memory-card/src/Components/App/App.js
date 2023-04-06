d4bab54b6b01d25774587929321450b91f9f32df
pritalb <pritalbamnodkar@gmail.com>
2023-04-06 15:48:15 +0530

    created card styling

--- memory-card/src/Components/App/App.js
+++ memory-card/src/Components/App/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Card from '../Card/Card';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello
+        <Card cardName="Space Ishtar" cardImageSource="/Assets/Images/spaceIshtar.jpg" />
       </header>
     </div>
   );
--- memory-card/src/Components/Card/Card.js
+++ memory-card/src/Components/Card/Card.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './Card.css';
 
 const Card = ({cardName, cardImageSource}) => {
     return (
         <div className='card'>
-            <div className='card-name'> { cardName } </div>
-            <div>
+            <div className='card-image-container'>
                 <img className='card-image' src={cardImageSource} /> 
             </div>
+            <div className='card-name'> { cardName } </div>
         </div>
     );
 };
