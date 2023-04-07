65795a81d11ba11db1e914cebc9e0a2c3f368893
pritalb <pritalbamnodkar@gmail.com>
2023-04-07 15:34:49 +0530

    Card component now flips on being clicked

--- memory-card/src/Components/Card/Card.js
+++ memory-card/src/Components/Card/Card.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 
 const Card = ({cardName, cardImageSource}) => {
+    const [isCardFlipped, setIsCardFlipped] = useState(false);
+
     const onClick = () => {
         console.log("clicked");
+        setIsCardFlipped(!isCardFlipped);
     }
 
     return (
-        <div className='card' onClick={onClick}>
-            <div className='card-front'>
-                <div className='card-front-image-container'>
-                    <img src='/Assets/Images/FGOMagicCircle.png' className='card-front-logo'/>
+        <div className={isCardFlipped ? 'card flipped' : 'card'} onClick={onClick}>
+            <div className='card-sides-container'>
+                <div className='card-front'>
+                    <div className='card-front-image-container'>
+                        <img src='/Assets/Images/FGOMagicCircle.png' className='card-front-logo'/>
+                    </div>
+                    <div className='card-front-text'>
+                        Fate/Grand Order
+                    </div>
                 </div>
-                <div className='card-front-text'>
-                    Fate/Grand Order
+                <div className='card-back'>
+                    <div className='card-image-container'>
+                        <img className='card-image' src={cardImageSource} /> 
+                    </div>
+                    <div className='card-name'> { cardName } </div>
                 </div>
-            </div>
-            <div className='card-back'>
-                <div className='card-image-container'>
-                    <img className='card-image' src={cardImageSource} /> 
-                </div>
-                <div className='card-name'> { cardName } </div>
             </div>
         </div>
     );
