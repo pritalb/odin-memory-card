0cbf6a465987634b0f0154218fb96a45232d3e0e
pritalb <pritalbamnodkar@gmail.com>
2023-04-07 14:58:12 +0530

    created front and back sides of Card Component

--- memory-card/src/Components/Card/Card.js
+++ memory-card/src/Components/Card/Card.js
@@ -2,12 +2,26 @@ import React from 'react';
 import './Card.css';
 
 const Card = ({cardName, cardImageSource}) => {
+    const onClick = () => {
+        console.log("clicked");
+    }
+
     return (
-        <div className='card'>
-            <div className='card-image-container'>
-                <img className='card-image' src={cardImageSource} /> 
+        <div className='card' onClick={onClick}>
+            <div className='card-front'>
+                <div className='card-front-image-container'>
+                    <img src='/Assets/Images/FGOMagicCircle.png' className='card-front-logo'/>
+                </div>
+                <div className='card-front-text'>
+                    Fate/Grand Order
+                </div>
             </div>
-            <div className='card-name'> { cardName } </div>
+            <div className='card-back'>
+                <div className='card-image-container'>
+                    <img className='card-image' src={cardImageSource} /> 
+                </div>
+                <div className='card-name'> { cardName } </div>
+            </div>
         </div>
     );
 };
