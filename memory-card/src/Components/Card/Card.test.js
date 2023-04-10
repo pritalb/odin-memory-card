1a6423729ef8d867e69e8bee5d245ee2dd944070
pritalb <pritalbamnodkar@gmail.com>
2023-04-10 14:54:25 +0530

    modified Card tests

--- memory-card/src/Components/Card/Card.test.js
+++ memory-card/src/Components/Card/Card.test.js
@@ -2,15 +2,13 @@ import { render, screen } from '@testing-library/react';
 import Card from "./Card";
 
 describe("Card component renders", () => {
-    test("Component contains all required fields: card name", () => {
-        const { container } = render(<Card cardName="Some Card" cardImageSource="#"/>);
+    const { container } = render(<Card cardName="Some Card" cardImageSource="#"/>);
 
+    test("Component contains all required fields: card name", () => {
         expect(container.querySelector(".card-name")).toBeInTheDocument();
     });
 
     test("Component contains all required fields: card image", () => {
-        const { container } = render(<Card cardName="Some Card" cardImageSource="#" />);
-
         expect(container.querySelector(".card-image")).toBeInTheDocument();
     });
 });
---
+++ memory-card/src/Components/GameBoard/GameBoard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GameBoard = () => {
+    return (
+        <div id='game-board'>
+
+        </div>
+    )
+}
+
+export default GameBoard;
