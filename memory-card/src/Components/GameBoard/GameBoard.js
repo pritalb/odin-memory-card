12f328762a08285f2bc996793629868fa3879e5a
pritalb <pritalbamnodkar@gmail.com>
2023-04-24 21:30:50 +0530

    cardsArray now stores objects with component information rather than the card components themselves

--- memory-card/src/Components/GameBoard/GameBoard.js
+++ memory-card/src/Components/GameBoard/GameBoard.js
@@ -3,18 +3,61 @@ import './GameBoard.css';
 import Card from '../Card/Card.js';
 
 const GameBoard = () => {
+    // const [cardsArray, setCardsArray] = useState([
+    //     <Card cardName="Space Ishtar" cardImageSource="/Assets/Images/spaceIshtar.jpg" />,
+    //     <Card cardName="Mashu Kyrielight" cardImageSource="/Assets/Images/mash.jpg" />,
+    //     <Card cardName="Musashi Miyamoto" cardImageSource="/Assets/Images/musashi.jpg" />,
+    //     <Card cardName="Ushiwakamaru" cardImageSource="/Assets/Images/ushi.png" />,
+    //     <Card cardName="Anastasia" cardImageSource="/Assets/Images/anastasia.jpg" />,
+    //     <Card cardName="Quetzalcoatl" cardImageSource="/Assets/Images/quetzalcoatl.jpg" />,
+    //     <Card cardName="BB" cardImageSource="/Assets/Images/bb.jpg" />,
+    //     <Card cardName="Artoria Caster" cardImageSource="/Assets/Images/artoria caster.jpg" />,
+    //     <Card cardName="Morgan Le Fae" cardImageSource="/Assets/Images/morgan.jpg" />,
+    //     <Card cardName="Ereshkigal" cardImageSource="/Assets/Images/ereshkigal.jpg" />,
+    // ]);
+
     const [cardsArray, setCardsArray] = useState([
-        <Card cardName="Space Ishtar" cardImageSource="/Assets/Images/spaceIshtar.jpg" />,
-        <Card cardName="Mashu Kyrielight" cardImageSource="/Assets/Images/mash.jpg" />,
-        <Card cardName="Musashi Miyamoto" cardImageSource="/Assets/Images/musashi.jpg" />,
-        <Card cardName="Ushiwakamaru" cardImageSource="/Assets/Images/ushi.png" />,
-        <Card cardName="Anastasia" cardImageSource="/Assets/Images/anastasia.jpg" />,
-        <Card cardName="Quetzalcoatl" cardImageSource="/Assets/Images/quetzalcoatl.jpg" />,
-        <Card cardName="BB" cardImageSource="/Assets/Images/bb.jpg" />,
-        <Card cardName="Artoria Caster" cardImageSource="/Assets/Images/artoria caster.jpg" />,
-        <Card cardName="Morgan Le Fae" cardImageSource="/Assets/Images/morgan.jpg" />,
-        <Card cardName="Ereshkigal" cardImageSource="/Assets/Images/ereshkigal.jpg" />,
-    ]);
+        {
+            name: "Space Ishtar",
+            image: "/Assets/Images/spaceIshtar.jpg",
+        },
+        {
+            name: "Mashu Kyrielight",
+            image: "/Assets/Images/mash.jpg" ,
+        },
+        {
+            name: "Musashi Miyamoto",
+            image: "/Assets/Images/musashi.jpg",
+        },
+        {
+            name: "Ushiwakamaru",
+            image: "/Assets/Images/ushi.png",
+        },
+        {
+            name: "Anastasia",
+            image: "/Assets/Images/anastasia.jpg",
+        },
+        {
+            name: "Quetzalcoatl",
+            image: "/Assets/Images/quetzalcoatl.jpg",
+        },
+        {
+            name: "BB",
+            image: "/Assets/Images/bb.jpg",
+        },
+        {
+            name: "Artoria Caster",
+            image: "/Assets/Images/artoria caster.jpg",
+        },
+        {
+            name: "Morgan le Fae",
+            image: "/Assets/Images/morgan.jpg",
+        },
+        {
+            name: "Ereshkigal",
+            image: "/Assets/Images/ereshkigal.jpg",
+        },
+    ])
 
     return (
         <div id='game-board'>
@@ -29,7 +72,9 @@ const GameBoard = () => {
                 <Card cardName="Space Ishtar" cardImageSource="/Assets/Images/spaceIshtar.jpg"/>
                 </div>
                 <div className='game-play-area'>
-                    { cardsArray }
+                    { cardsArray.map((card) => {
+                        return <Card cardName={card.name} cardImageSource={card.image} />;
+                    })}
                 </div>
             </div>
         </div>
